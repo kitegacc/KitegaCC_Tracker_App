a4685abbd99a89edad34d941266e2c84e143f691
Philip Liberato <pnl8zp@virginia.edu>
2016-06-22 20:08:08 -0400

    app can now add members

--- InformationTracker/app/src/main/java/org/kitegacc/information_tracker/CreateFormActivity.java
+++ InformationTracker/app/src/main/java/org/kitegacc/information_tracker/CreateFormActivity.java
@@ -1,11 +1,19 @@
 package org.kitegacc.information_tracker;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -15,6 +23,9 @@ public class CreateFormActivity extends AppCompatActivity {
     private String FORM_TYPE = "";
     private int NUM_FIELDS = 0;
     private HashMap<String, String> QUERY_ARGS;
+    private ProgressDialog pDialog;
+    JSONParserHTTPRequest jphtr = new JSONParserHTTPRequest();
+    private static String url_create_element = "http://androidapp.davidclemy.org/create_element.php";
 
     private EditText FORM_FIELD_1;
     private EditText FORM_FIELD_2;
@@ -113,19 +124,28 @@ public class CreateFormActivity extends AppCompatActivity {
     }
 
     public void createCommunityAccountForm() {
+        QUERY_ARGS.put("form_type", "community");
         setTitle("Create Community Account");
     }
 
-    public void submitCreateForm() {
-        switch (FORM_TYPE) {
-            case "member":
-                submitCreateMemberForm();
-                break;
-            case "community":
-                submitCreateCommunityAccountForm();
-                break;
-            default:
-                break;
+    public void submitCreateForm(View view) {
+        if(isFormComplete()) {
+            switch (FORM_TYPE) {
+                case "member":
+                    submitCreateMemberForm();
+                    break;
+                case "community":
+                    submitCreateCommunityAccountForm();
+                    break;
+                case "meeting":
+                    break;
+                case "loan":
+                    break;
+                case "payment":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -135,10 +155,134 @@ public class CreateFormActivity extends AppCompatActivity {
         QUERY_ARGS.put("emergency_contact", FORM_FIELD_3.getText().toString());
         QUERY_ARGS.put("residence", FORM_FIELD_4.getText().toString());
         QUERY_ARGS.put("kin_or_spouse", FORM_FIELD_5.getText().toString());
-
+        new FormPoster().execute();
     }
 
     public void submitCreateCommunityAccountForm() {
+
+    }
+
+    public boolean isFormComplete() {
+        if(NUM_FIELDS >= 1) {
+            if(FORM_FIELD_1.getText().toString().trim().equals("")) return false;
+        }
+        if(NUM_FIELDS >= 2) {
+            if(FORM_FIELD_2.getText().toString().trim().equals("")) return false;
+        }
+        if(NUM_FIELDS >= 3) {
+            if(FORM_FIELD_3.getText().toString().trim().equals("")) return false;
+        }
+        if(NUM_FIELDS >= 4) {
+            if(FORM_FIELD_4.getText().toString().trim().equals("")) return false;
+        }
+        if(NUM_FIELDS >= 5) {
+            if(FORM_FIELD_5.getText().toString().trim().equals("")) return false;
+        }
+        if(NUM_FIELDS >= 6) {
+            if(FORM_FIELD_6.getText().toString().trim().equals("")) return false;
+        }
+        if(NUM_FIELDS >= 7) {
+            if(FORM_FIELD_7.getText().toString().trim().equals("")) return false;
+        }
+        return true;
+    }
+    /**
+     * Background Async Task to Load all members by making HTTP Request
+     * */
+    class FormPoster extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(CreateFormActivity.this);
+            pDialog.setMessage("Submitting form. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+//            HashMap<String, String> params = new HashMap<>();
+//            params.put("community_id", Integer.toString(COMMUNITY_ID));
+//            // getting JSON string from URL
+            JSONObject json = jphtr.makeHttpRequest(url_create_element, "GET", QUERY_ARGS);
+
+            // Check your log cat for JSON response
+            Log.d("All Products: ", json.toString());
+//
+//            try {
+//                // Checking for SUCCESS TAG
+//                int success = json.getInt(TAG_SUCCESS);
+//
+//                if (success == 1) {
+//                    // products found
+//                    // Getting Array of Products
+//                    members = json.getJSONArray(TAG_MEMBERS);
+//
+//                    // looping through All Products
+//                    for (int i = 0; i < members.length(); i++) {
+//                        JSONObject c = members.getJSONObject(i);
+//
+//                        // Storing each json item in variable
+//                        String id = c.getString(TAG_MID);
+//                        String name = c.getString(TAG_NAME);
+//
+//                        // creating new HashMap
+//                        HashMap<String, String> map = new HashMap<String, String>();
+//
+//                        // adding each child node to HashMap key => value
+//                        map.put(TAG_MID, id);
+//                        map.put(TAG_NAME, name);
+//
+//                        // adding HashList to ArrayList
+//                        membersList.add(map);
+//                    }
+//                } /*else {
+//                    // no products found
+//                    // Launch Add New product Activity
+//                    Intent i = new Intent(getApplicationContext(),
+//                            NewProductActivity.class);
+//                    // Closing all previous activities
+//                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(i);
+//                }*/
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+//            runOnUiThread(new Runnable() {
+//                public void run() {
+//                    /**
+//                     * Updating parsed JSON data into ListView
+//                     * */
+//                    ListAdapter adapter = new SimpleAdapter(
+//                            CommunityMembersList.this, membersList,
+//                            R.layout.member_list_item, new String[] { TAG_MID,
+//                            TAG_NAME},
+//                            new int[] { R.id.member_id_list_item, R.id.member_name_list_item });
+//                    // updating listview
+//                    setListAdapter(adapter);
+//                }
+//            });
+
+        }
 
     }
 }
